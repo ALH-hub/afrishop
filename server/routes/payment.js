0934ad21a61703171ad77ce142b54c51a6060630
Alhadji Oumate <alhadjioum0@gmail.com>
2024-07-29 00:37:54 +0100

    feat: route creation for the different controllers

---
+++ server/routes/payment.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  getPayment,
+  getPayments,
+  createPayment,
+  updatePayment,
+  deletePayment,
+} from '../controllers/payment';
+
+const router = express.Router();
+
+router.get('/', getPayments);
+router.get('/:id', getPayment);
+router.post('/', createPayment);
+router.put('/:id', updatePayment);
+router.delete('/:id', deletePayment);
+
+export default router;
