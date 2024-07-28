4968cdeb0703dc37f6fe3fd7011a74e803740d6e
Alhadji Oumate <alhadjioum0@gmail.com>
2024-07-29 00:24:37 +0100

    feat: database crud methods for the different collections

--- server/utils/db.js
+++ server/utils/db.js
@@ -52,6 +52,15 @@ class DBClient {
     }
   }
 
+  async findClients() {
+    try {
+      return await this.db.collection('clients').find();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
   async updateClient(client, body) {
     try {
       return await this.db.collection('clients').updateOne(client, {
@@ -113,6 +122,194 @@ class DBClient {
       return await this.db.collection('admins').updateOne(admin, {
         $set: body,
       });
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async deleteAdmin(id) {
+    try {
+      return await this.db.collection('admins').deleteOne(id);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async findVendor(id) {
+    try {
+      return await this.db.collection('vendors').findOne(id);
+    } catch (error) {
+      console.error('Error finding the vendor: ', error);
+      throw error;
+    }
+  }
+
+  async findVendors() {
+    try {
+      return await this.db.collection('vendors').find();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async updateVendor(vendor, body) {
+    try {
+      return await this.db.collection('vendors').updateOne(vendor, {
+        $set: body,
+      });
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async deleteVendor(id) {
+    try {
+      return await this.db.collection('vendors').deleteOne(id);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async insertOrder(order) {
+    try {
+      return await this.db.collection('orders').insertOne(order);
+    } catch (error) {
+      console.error('Error creating order: ', error);
+      throw error;
+    }
+  }
+
+  async findOrder(id) {
+    try {
+      return await this.db.collection('orders').findOne(id);
+    } catch (error) {
+      console.error('Error finding the order: ', error);
+      throw error;
+    }
+  }
+
+  async findOrders() {
+    try {
+      return await this.db.collection('orders').find();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async updateOrder(order, body) {
+    try {
+      return await this.db.collection('orders').updateOne(order, {
+        $set: body,
+      });
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async deleteOrder(id) {
+    try {
+      return await this.db.collection('orders').deleteOne(id);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async insertPayment(payment) {
+    try {
+      return await this.db.collection('payments').insertOne(payment);
+    } catch (error) {
+      console.error('Error creating payment: ', error);
+      throw error;
+    }
+  }
+
+  async findPayment(id) {
+    try {
+      return await this.db.collection('payments').findOne(id);
+    } catch (error) {
+      console.error('Error finding the payment: ', error);
+      throw error;
+    }
+  }
+
+  async findPayments() {
+    try {
+      return await this.db.collection('payments').find();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async updatePayment(payment, body) {
+    try {
+      return await this.db.collection('payments').updateOne(payment, {
+        $set: body,
+      });
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async deletePayment(id) {
+    try {
+      return await this.db.collection('payments').deleteOne(id);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async insertProduct(product) {
+    try {
+      return await this.db.collection('products').insertOne(product);
+    } catch (error) {
+      console.error('Error creating product: ', error);
+      throw error;
+    }
+  }
+
+  async findProduct(id) {
+    try {
+      return await this.db.collection('products').findOne(id);
+    } catch (error) {
+      console.error('Error finding the product: ', error);
+      throw error;
+    }
+  }
+
+  async findProducts() {
+    try {
+      return await this.db.collection('products').find();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async updateProduct(product, body) {
+    try {
+      return await this.db.collection('products').updateOne(product, {
+        $set: body,
+      });
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async deleteProduct(id) {
+    try {
+      return await this.db.collection('products').deleteOne(id);
     } catch (error) {
       console.error(error);
       throw error;
