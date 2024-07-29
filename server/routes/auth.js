b8b59e5e153a8ac149943e07e833875af6b82ba9
Alex noumbi <alenoumbi@gmail.com>
2024-07-29 06:18:36 +0100

    v

--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { authController } from '../controllers/auth.js';
-
-const router = express.Router();
-
-router.get('/', authController);
-
-module.exports = router;
