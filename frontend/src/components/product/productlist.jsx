7c1b8171b5b53aff9bf55443568007ce42b1329b
Alex noumbi <alenoumbi@gmail.com>
2024-07-29 06:45:21 +0100

    nn

--- frontend/src/components/product/productlist.jsx
+++ frontend/src/components/product/productlist.jsx
@@ -10,7 +10,7 @@ const ProductsList = () => {
     // Fonction pour récupérer les produits
     const fetchProducts = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/api/products');
+        const response = await axios.get('http://localhost:3030/api/products');
         setProducts(response.data);
       } catch (error) {
         setError(error.message);
