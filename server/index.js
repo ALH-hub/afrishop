26a55f2086cc6ad3a35083eff25f2360076fe5e0
Alhadji Oumate <alhadjioum0@gmail.com>
2024-07-29 06:46:02 +0100

    refactor: port number modification

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use('/api/review', reviewRouter);
 app.use('/api/payment', paymentRouter);
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3030');
+  console.log('Server is running on port 3000');
 });
 
 export default app;
