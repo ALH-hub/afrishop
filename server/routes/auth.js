3a77efddfd41c1439721500a43ed1bf7db53988b
Alhadji Oumate <alhadjioum0@gmail.com>
2024-07-29 06:07:49 +0100

    refactor: auth files modification

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,35 @@
 import express from 'express';
-import authController from '../controllers/auth.js';
+import {
+  adminLogin,
+  adminRegister,
+  logout,
+  clientLogin,
+  clientRegister,
+  vendorLogin,
+  vendorRegister,
+  adminGetme,
+  clientGetme,
+  vendorGetme,
+} from '../controllers/auth.js';
 
 const router = express.Router();
 
-router.get('/', authController);
+// client
+router.post('/client/register', clientRegister);
+router.post('/client/login', clientLogin);
+
+// vendor
+router.post('/vendor/register', vendorRegister);
+router.post('/vendor/login', vendorLogin);
+
+// admin
+router.post('/admin/register', adminRegister);
+router.post('/admin/login', adminLogin);
+
+router.get('/client/me', clientGetme);
+router.get('/vendor/me', vendorGetme);
+router.get('/admin/me', adminGetme);
+
+router.post('/logout', logout);
 
 export default router;
